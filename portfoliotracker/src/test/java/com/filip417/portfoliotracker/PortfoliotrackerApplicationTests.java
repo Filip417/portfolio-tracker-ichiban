1eef39af4e262a599947ce63699a5923c0ea708b
Filip417 <sawinskif@gmail.com>
2025-08-21 20:24:02 +0200

    spring boot initialised

---
+++ portfoliotracker/src/test/java/com/filip417/portfoliotracker/PortfoliotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filip417.portfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfoliotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
